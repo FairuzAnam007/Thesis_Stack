f533ae9349159f3954a65eb2ff27f7403587aedd
Fairuz Anam <23101122@uap-bd.edu>
2025-10-11 23:08:03 +0600

    Initial commit

---
+++ thesisstack/thesisstack/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for thesisstack project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'thesisstack.settings')
+
+application = get_asgi_application()
