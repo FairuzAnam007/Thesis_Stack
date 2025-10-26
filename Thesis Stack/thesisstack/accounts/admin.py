7f30f59c546d773fa38caa856cf41816ff9cb91a
Labib-1008 <fm.labib1008@gmail.com>
2025-10-27 01:26:11 +0600

    admin.py

--- Thesis Stack/thesisstack/accounts/admin.py
+++ Thesis Stack/thesisstack/accounts/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import StudentProfile, ThesisGroup, ThesisSubmission
+
+admin.site.register(StudentProfile)
+admin.site.register(ThesisGroup)
+admin.site.register(ThesisSubmission)
